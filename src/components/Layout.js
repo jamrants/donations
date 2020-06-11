418b2cad37005142644baadf29daf8264f24a251
jhthenerd <jhthenerd@users.noreply.github.com>
2020-06-11 21:45:39 +0000

    Prettified Code!

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Box, PseudoBox } from '@chakra-ui/core';
-import { IsDesktop } from '../utils/mediaQueries';
-import Header from './Header';
-import Hero from './Hero';
-import Footer from './Footer';
+import React from "react"
+import PropTypes from "prop-types"
+import { Box, PseudoBox } from "@chakra-ui/core"
+import { IsDesktop } from "../utils/mediaQueries"
+import Header from "./Header"
+import Hero from "./Hero"
+import Footer from "./Footer"
 
 const Layout = ({ children, title, subtitle }) => (
-  <Box
-    backgroundColor='dark'
-    minHeight='100vh'
-  >
-    <Hero 
-      title={title} 
+  <Box backgroundColor="dark" minHeight="100vh">
+    <Hero
+      title={title}
       subtitle={subtitle}
-      padding={['24px', null, null, '17%', '20%']}
+      padding={["24px", null, null, "17%", "20%"]}
     />
-    <PseudoBox px={['24px', null, null, '17%', '20%']}>
-      { children }
-    </PseudoBox>
-    <Footer padding={['24px', null, null, '17%', '20%']}/>
+    <PseudoBox px={["24px", null, null, "17%", "20%"]}>{children}</PseudoBox>
+    <Footer padding={["24px", null, null, "17%", "20%"]} />
   </Box>
 )
 
-export default Layout;
+export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,51 +1,84 @@
-import React, { useState, useEffect } from 'react';
-import { graphql } from 'gatsby';
-import Layout from '../components/Layout';
-import { Box, Text, DarkMode } from '@chakra-ui/core';
-import SearchField from '../components/Search/Search';
-import '../components/Search/search.css';
-import DonationCard from '../components/donationCard';
+import React, { useState, useEffect } from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import { Box, Text, DarkMode } from "@chakra-ui/core"
+import SearchField from "../components/Search/Search"
+import "../components/Search/search.css"
+import DonationCard from "../components/donationCard"
 
 const Home = ({ data }) => {
-
-  const [corporations, setCorporations] = useState(data.allAirtable.nodes);
-  const [filteredCorporations, setFilteredCorporations] = useState(data.allAirtable.nodes);
+  const [corporations, setCorporations] = useState(data.allAirtable.nodes)
+  const [filteredCorporations, setFilteredCorporations] = useState(
+    data.allAirtable.nodes
+  )
 
   // Search Field
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState("")
 
-  const searchFieldOnChange = (e) => {
-    setSearchValue(e.target.value);
-    setFilteredCorporations(corporations.filter(corporation => corporation.data.Name.toLowerCase().includes(e.target.value.toLowerCase())))
+  const searchFieldOnChange = e => {
+    setSearchValue(e.target.value)
+    setFilteredCorporations(
+      corporations.filter(corporation =>
+        corporation.data.Name.toLowerCase().includes(
+          e.target.value.toLowerCase()
+        )
+      )
+    )
   }
 
   return (
     <DarkMode>
-      <Layout title='Donations Exposed' subtitle='Corporations have made headlines with big pledges recently — how much would they be to the average person?'>
-        <Box py='64px'>
+      <Layout
+        title="Donations Exposed"
+        subtitle="Corporations have made headlines with big pledges recently — how much would they be to the average person?"
+      >
+        <Box py="64px">
           <Box
-            textAlign='center'
-            display='flex'
-            flexDirection='column'  
-            alignItems='center'
-            justifyContent='center'
+            textAlign="center"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
           >
-            <Text color='primary.red' mb={['16px', null, null, null, '24px']} fontWeight='900' fontSize={['24px', '26px', '28px',  '30px', '32px']}>
+            <Text
+              color="primary.red"
+              mb={["16px", null, null, null, "24px"]}
+              fontWeight="900"
+              fontSize={["24px", "26px", "28px", "30px", "32px"]}
+            >
               Find a Corporation
             </Text>
-            <SearchField placeholder="Amazon" value={searchValue} onChange={searchFieldOnChange} />
+            <SearchField
+              placeholder="Amazon"
+              value={searchValue}
+              onChange={searchFieldOnChange}
+            />
           </Box>
         </Box>
-        <Box 
-          pb='64px'
-          display='grid'
-          gridTemplateColumns={['1fr', '1fr', '1fr', '1fr 1fr']}
-          gridColumnGap={['20px', '20px', '32px', '40px', '48px']}
-          gridRowGap={['20px', '20px', '32px  ', '40px', '48px']}
+        <Box
+          pb="64px"
+          display="grid"
+          gridTemplateColumns={["1fr", "1fr", "1fr", "1fr 1fr"]}
+          gridColumnGap={["20px", "20px", "32px", "40px", "48px"]}
+          gridRowGap={["20px", "20px", "32px  ", "40px", "48px"]}
         >
           {filteredCorporations.map(corporation => {
-            if (corporation.data.Donation__thousands_ && corporation.data.Gross_Profit__millions_) {
-              return <DonationCard imageURL={corporation.data.Logo && corporation.data.Logo[0].url} name={corporation.data.Name} percent={corporation.data.Percent_Profits} amount={corporation.data.Donation__thousands_} donationCurrency={corporation.data.Currency} profits={corporation.data.Gross_Profit__millions_}/>
+            if (
+              corporation.data.Donation__thousands_ &&
+              corporation.data.Gross_Profit__millions_
+            ) {
+              return (
+                <DonationCard
+                  imageURL={
+                    corporation.data.Logo && corporation.data.Logo[0].url
+                  }
+                  name={corporation.data.Name}
+                  percent={corporation.data.Percent_Profits}
+                  amount={corporation.data.Donation__thousands_}
+                  donationCurrency={corporation.data.Currency}
+                  profits={corporation.data.Gross_Profit__millions_}
+                />
+              )
             }
           })}
         </Box>
@@ -56,7 +89,10 @@ const Home = ({ data }) => {
 
 export const query = graphql`
   query corporationsQuery {
-    allAirtable(filter: {table: {eq: "Corporations"}}, sort: {fields: data___Donation__thousands_, order: DESC}) {
+    allAirtable(
+      filter: { table: { eq: "Corporations" } }
+      sort: { fields: data___Donation__thousands_, order: DESC }
+    ) {
       nodes {
         data {
           Currency
