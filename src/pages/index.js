6eee9953a3bbbedf63aaa5405b4b49ff21b2a1d7
David Li <david.li3100@gmail.com>
2020-06-11 19:26:38 -0400

    add dynamic locales

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react"
 
 import { graphql } from "gatsby"
 import Layout from "../components/Layout"
-import { Box, Text, DarkMode } from "@chakra-ui/core"
+import { Box, Text, DarkMode, Skeleton } from "@chakra-ui/core"
 import SearchField from "../components/Search/Search"
 import "../components/Search/search.css"
 import DonationCard from "../components/donationCard"
+
+const Home = ({ data }) => {
 
   const [corporations, setCorporations] = useState(data.allAirtable.nodes);
   const [filteredCorporations, setFilteredCorporations] = useState(data.allAirtable.nodes);
@@ -29,7 +31,7 @@ import DonationCard from "../components/donationCard"
   useEffect(() => {
     // get user country, sort through locales + income to adjust
     const locale = navigator.language || navigator.browserLanguage || ( navigator.languages || [ "en" ] )[0];
-    locale ? setActiveLocale(localeList.filter(l => l.Languages.split(",").includes(locale))[0]) : setActiveLocale(localeList.filter(l => l.Languages.split(",").includes('en-US'))[0]);
+    locale ? setActiveLocale(localeList.filter(l => l.Locales.split(",").includes(locale))[0]) : setActiveLocale(localeList.filter(l => l.Locales.split(",").includes('en-US'))[0]);
   }, [data])
 
   return (
@@ -68,25 +70,39 @@ import DonationCard from "../components/donationCard"
           gridColumnGap={["20px", "20px", "32px", "40px", "48px"]}
           gridRowGap={["20px", "20px", "32px  ", "40px", "48px"]}
         >
-          {filteredCorporations.map(corporation => {
-            if (
-              corporation.data.Donation__thousands_ &&
-              corporation.data.Gross_Profit__millions_
-            ) {
-              return (
-                <DonationCard
-                  image={
-                    corporation.data.Logo.localFiles[0].childImageSharp.fixed
-                  }
-                  name={corporation.data.Name}
-                  percent={corporation.data.Percent_Profits}
-                  amount={corporation.data.Donation__thousands_}
-                  donationCurrency={corporation.data.Currency}
-                  profits={corporation.data.Gross_Profit__millions_}
-                />
-              )
-            }
-          })}
+          {activeLocale.Currency ? 
+            filteredCorporations.map(corporation => {
+              if (
+                corporation.data.Donation__thousands_ &&
+                corporation.data.Gross_Profit__millions_
+              ) {
+                return (
+                  <DonationCard
+                    locale={activeLocale}
+                    image={
+                      corporation.data.Logo.localFiles[0].childImageSharp.fixed
+                    }
+                    name={corporation.data.Name}
+                    percent={corporation.data.Percent_Profits}
+                    amount={corporation.data.Donation__thousands_}
+                    donationCurrency={corporation.data.Currency}
+                    profits={corporation.data.Gross_Profit__millions_}
+                  />
+                )
+              }
+            })         
+            :
+            <>
+              <Skeleton h="220px" borderRadius="10px"/>
+              <Skeleton h="220px" borderRadius="10px"/>
+              <Skeleton h="220px" borderRadius="10px"/>
+              <Skeleton h="220px" borderRadius="10px"/>
+              <Skeleton h="220px" borderRadius="10px"/>
+              <Skeleton h="220px" borderRadius="10px"/>
+              <Skeleton h="220px" borderRadius="10px"/>
+              <Skeleton h="220px" borderRadius="10px"/>
+            </>   
+          }
         </Box>
       </Layout>
     </DarkMode>
@@ -131,10 +147,11 @@ export const query = graphql`
       edges {
         node {
           data {
-            Languages
             Location
             Median_Household_Income
+            Language
             Currency
+            Locales
           }
         }
       }
