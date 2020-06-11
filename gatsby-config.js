5c383918ebcd3ede23db55cf8e19b42fac687ae0
David Li <david.li3100@gmail.com>
2020-06-11 11:37:20 -0400

    add airtable source

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,20 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-source-airtable',
+      options: {
+        apiKey: process.env.AIRTABLE_KEY,
+        tables: [
+          {
+            baseId: process.env.BASE_ID,
+            tableName: `Corporations`
+          },
+        ]
+      }
+    }
+  ],
 }
