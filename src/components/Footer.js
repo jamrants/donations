1f53a23a88a13a24cb365d18c898b0fdff1856f4
David Li <david.li3100@gmail.com>
2020-06-12 14:36:38 -0400

    adding about page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -71,7 +71,7 @@ const Footer = ({ padding }) => (
       >
         GunFunded.com
       </PseudoBox>
-      {" and "}
+      {" & "}
       <PseudoBox
         color="primary.green"
         fontSize="16px"
