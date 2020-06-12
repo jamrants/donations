296f64f985a1c5ad478798dcd40579a580ca2755
David Li <david.li3100@gmail.com>
2020-06-12 12:08:44 -0400

    fix responsive styling

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,14 +6,26 @@ import Header from "./Header"
 import Hero from "./Hero"
 import Footer from "./Footer"
 
+const breakpoints = ["30em", "48em", "62em", "65.9375em", "75em", "107em"]
+//1200px - 7%
+//1055px -  5%
+// 62em - 20% - hero - 10%  
+breakpoints.xs = breakpoints[0]
+breakpoints.sm = breakpoints[1]
+breakpoints.md = breakpoints[2]
+breakpoints.lg = breakpoints[3]
+breakpoints.xl = breakpoints[4]
+breakpoints.xxl = breakpoints[5]
+
+
 const Layout = ({ children, title, subtitle }) => (
-  <Box backgroundColor="dark" minHeight="100vh">
+  <Box backgroundColor="dark" minHeight="100vh">  
     <Hero
       title={title}
       subtitle={subtitle}
       padding={["24px", null, null, "17%", "20%"]}
     />
-    <PseudoBox px={["24px", null, null, "17%", "20%"]}>{children}</PseudoBox>
+    <PseudoBox className="content-container" >{children}</PseudoBox>
     <Footer padding={["24px", null, null, "17%", "20%"]} />
   </Box>
 )
