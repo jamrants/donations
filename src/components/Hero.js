51fda9eadddb42b14d15cd2c4db7d386142e64da
David Li <david.li3100@gmail.com>
2020-06-12 23:15:42 -0400

    styling - fix copy

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -20,7 +20,7 @@ const Hero = ({ padding, title, subtitle }) => {
         >
           {title}
         </Text>
-        <Text color="snow" fontSize={["20px", "24px", "28px", "31px", "38px"]}>
+        <Text color="snow" mt={["16px", "12px"]} fontSize={["20px", "24px", "28px", "31px", "38px"]}>
           {subtitle}
         </Text>
       </Box>
