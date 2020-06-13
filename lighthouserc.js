c80b2e03c73c5144635403b9b5e9345fec355d7d
Jason Huang <jasonhuang20035@gmail.com>
2020-06-13 16:57:08 -0400

    Update Config Again

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,8 @@
 module.exports = {
   ci: {
     collect: {
-      url: ["http://localhost/index.html", "http://localhost/about.html"],
+      url: ["http://localhost/index.html", "http://localhost/about/index.html"],
+      staticDistDir: "./public",
     },
     upload: {
       target: "temporary-public-storage",
