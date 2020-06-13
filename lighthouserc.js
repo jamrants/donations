26d5bb6b02338655d55576ee945b2e2ba1a07b3f
Jason Huang <jasonhuang20035@gmail.com>
2020-06-13 16:26:00 -0400

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,8 @@
 module.exports = {
   ci: {
+    collect: {
+      url: ["http://localhost/index.html", "http://localhost/about.html"],
+    },
     upload: {
       target: "temporary-public-storage",
     },
