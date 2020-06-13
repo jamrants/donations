b6c36d08b6fe88fa7a5e3ce4efe49b4b56ea506d
jhthenerd <jhthenerd@users.noreply.github.com>
2020-06-13 01:02:22 +0000

    Prettified Code!

--- src/components/FlagMenu.js
+++ src/components/FlagMenu.js
@@ -6,7 +6,11 @@ const renderMenuItems = (locales, onClick) => {
   return locales.map(locale => {
     return (
       <MenuItem py="4px" width="fit-content" onClick={() => onClick(locale)}>
-        <ReactCountryFlag style={{ height: "36px", width: "36px" }} countryCode={locale.Code} svg />
+        <ReactCountryFlag
+          style={{ height: "36px", width: "36px" }}
+          countryCode={locale.Code}
+          svg
+        />
       </MenuItem>
     )
   })
@@ -15,8 +19,18 @@ const renderMenuItems = (locales, onClick) => {
 const FlagMenu = ({ onClick, locales, activeLocale }) => {
   return (
     <Menu>
-      <MenuButton lineHeight="initial" pb="2px" px="4px" ml="4px" borderBottom="2.5px solid #F9FAFC">
-        <ReactCountryFlag style={{ height: "36px", width: "36px" }} countryCode={activeLocale.Code} svg />
+      <MenuButton
+        lineHeight="initial"
+        pb="2px"
+        px="4px"
+        ml="4px"
+        borderBottom="2.5px solid #F9FAFC"
+      >
+        <ReactCountryFlag
+          style={{ height: "36px", width: "36px" }}
+          countryCode={activeLocale.Code}
+          svg
+        />
         <Icon ml="6px" name="chevron_down" h="12px" />
       </MenuButton>
       <MenuList
