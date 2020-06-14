a9043306f42c09c0e80ed5e8455c3ccb2c1fe05b
James Ah Yong <james@retrocraft.ca>
2020-06-14 01:43:27 -0400

    key number labels

--- src/components/DonationSlider.js
+++ src/components/DonationSlider.js
@@ -5,6 +5,7 @@ import {
   SliderFilledTrack,
   SliderTrack,
   SliderThumb,
+  PseudoBox,
 } from "@chakra-ui/core"
 
 let formatter = new Intl.NumberFormat(undefined, {
@@ -16,6 +17,9 @@ const logScale = (min, max, x) => {
   const n = 1.0 - Math.log(min) / Math.log(max)
   return min * Math.pow(max, (n * x) / 100)
 }
+const invLogScale = (min, max, x) => {
+  return 1.0 - Math.log(x / max) / Math.log(min / max)
+}
 const fakeLog = x => {
   if (0 <= x && x <= 25) return x
   else if (25 < x && x <= 75) return 1.5 * x - 12.5
@@ -25,21 +29,28 @@ const fakeLog = x => {
 const DonationSlider = ({ locale, corporations }) => {
   const [rawValue, setRawValue] = useState(0)
   // it breaks for some reason if this isn't stored in state AND I DONT KNOW WHY
-  const [corps, setCorps] = useState(corporations)
+  const [corps] = useState(corporations)
   const min = Number(
     (locale.Income * corps[corps.length - 1].Percent_Profits).toPrecision(1)
   )
   const max = Number((locale.Income * corps[0].Percent_Profits).toPrecision(1))
   const value = logScale(min, max, rawValue)
-  const formattedValue = new Intl.NumberFormat(undefined, {
+  const formatter = new Intl.NumberFormat(undefined, {
     style: "currency",
     currency: locale.Currency,
-  }).format(value)
+  })
+  const intFormatter = new Intl.NumberFormat(undefined, {
+    style: "currency",
+    currency: locale.Currency,
+    minimumFractionDigits: 0,
+  })
+  const formattedValue = formatter.format(value)
   const corp = corps.find(_ => value > _.Percent_Profits * locale.Income)
   const notches = []
-  for (let i = 1; ; i++) {
+  for (let i = 0; ; i++) {
     const notch =
-      Math.pow(2, (i - 1) % 2) * 5 * Math.pow(10, Math.floor((i - 1) / 2)) // OEIS A268100
+      Math.pow(2, (i - 1) % 2) * 5 * Math.pow(10, Math.floor((i - 1) / 2)) // OEIS A268100 (1/5/10...)
+      // (Math.pow(i % 3, 2) + 1) * Math.pow(10, Math.floor(i / 3)) // OEIS A051109 (1/2/5/10...)
     if (notch > max) break
     if (notch > min) notches.push(notch)
   }
@@ -53,11 +64,31 @@ const DonationSlider = ({ locale, corporations }) => {
         step={0.1}
         min={0}
         max={100}
-        mb="20px"
+        mb="30px"
       >
         <SliderTrack />
         <SliderFilledTrack bg="snow" />
         <SliderThumb bg="primary.green" />
+        <PseudoBox width="100%" position="absolute">
+          {notches.map(notch => {
+            const percent = invLogScale(min, max, notch) * 100
+            return (
+              <PseudoBox
+                as="span"
+                position="absolute"
+                top="10px"
+                left={`${percent}%`}
+                transform="translate(-50%, 0)"
+                fontSize="xs"
+                transition="all 250ms"
+                color={value >= notch ? "snow" : "darkless"}
+                onClick={() => setRawValue(Math.ceil(percent * 1000) / 1000)}
+              >
+                {intFormatter.format(notch)}
+              </PseudoBox>
+            )
+          })}
+        </PseudoBox>
       </Slider>
       <Button bg="primary.green" display="inline-block">
         Donate {formattedValue}!
