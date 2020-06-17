6b247e7cc8dd0e0448a2c1eacc102ba06ecd47f8
David Li <david.li3100@gmail.com>
2020-06-16 22:03:14 -0400

    add aria-label. Remove useless hidden label

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,28 +2,24 @@ import React from "react"
 import { Input } from "@chakra-ui/core"
 
 const SearchField = ({ value, onChange, placeholder }) => (
-  <>
-    <Input
-      id="find-corporation"
-      value={value}
-      onChange={onChange}
-      placeholder={placeholder}
-      size={["sm", "md", "lg"]}
-      width={["100%", "80%", "60%"]}
-      maxWidth="500px"
-      h="38px"
-      borderRadius="5px"
-      backgroundColor="darkless"
-      color="smoke"
-      border="none"
-      px="16px"
-      py="8px"
-      className="search-field"
-    />
-    <label for="find-corporation" hidden>
-      Search for a corporation
-    </label>
-  </>
+  <Input
+    id="find-corporation"
+    value={value}
+    onChange={onChange}
+    placeholder={placeholder}
+    size={["sm", "md", "lg"]}
+    width={["100%", "80%", "60%"]}
+    maxWidth="500px"
+    h="38px"
+    borderRadius="5px"
+    backgroundColor="darkless"
+    color="smoke"
+    border="none"
+    px="16px"
+    py="8px"
+    className="search-field"
+    aria-label="Search for a corporation"
+  />
 )
 
 export default SearchField
